ba26aba223f6ef29ee005448adc9a39e185797cf
Leandro Assis <leandrou9@gmail.com>
2022-02-19 09:34:48 -0300

    Atualização de operações matemáticas.

--- TesteCalculadora.java
+++ TesteCalculadora.java
@@ -1,30 +1,34 @@
 import java.util.*;
 public class TesteCalculadora {
     public static void main(String Args[]) {
+        int op1 = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Selecione qual operacao deseja realizar:\n1) Soma\n2) Subtracao\n3) Multiplicacao\n4) Divisao");
-        int op1 = sc.nextInt();
         do{
-            System.out.println("Digite dois valores:");
-            double n1 = sc.nextInt();
-            double n2 = sc.nextInt();
-
-            switch(op1){
-                case 1:
-                    System.out.println(n1+n2);
-                    break;
-                case 2:
-                    System.out.println(n1-n2);
-                    break;
-                case 3:
-                    System.out.println(n1*n2);
-                    break;
-                case 4:
-                    System.out.println(n1/n2);
-                    break;
-                case 5:
-                    sc.close();
+            System.out.println("\nSelecione qual operacao deseja realizar:\n1) Soma\n2) Subtracao\n3) Multiplicacao\n4) Divisao\n5) Sair");
+            op1 = sc.nextInt();
+            if(op1 != 5){
+                System.out.println("\nDigite dois valores:");
+                double n1 = sc.nextInt();
+                double n2 = sc.nextInt();
+                
+                switch(op1){
+                    case 1:
+                        System.out.printf("\nResultado = %f\n", n1+n2);
+                        break;
+                    case 2:
+                        System.out.printf("\nResultado = %f\n", n1-n2);
+                        break;
+                    case 3:
+                        System.out.printf("\nResultado = %f\n", n1*n2);
+                        break;
+                    case 4:
+                        System.out.printf("\nResultado = %f\n", n1/n2);
+                        break;
+                    case 5:
+                        sc.close();
+                }
             }
+            else sc.close();
         } while(op1 != 5);
     }
 }
